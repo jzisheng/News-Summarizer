aecfc53d834200103a7dcff8ffddb0f02280b631
jzisheng <jzisheng@gmail.com>
2020-10-12 18:03:29 +0000

    pushed latest version of website now supporting new streams. To do, add filters for keywords and positivity ranges git push

--- webService/routes/articles.js
+++ webService/routes/articles.js
@@ -1,8 +1,38 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./../models/article')
 const router = express.Router()
 
-router.get('/test',(req,res) =>{
-    res.send('in articles')
+router.get('/new',(req,res) =>{
+    res.render('articles/new',{ article:new Article()})
+})
+
+
+router.get('/:id',async (req,res) =>{
+    const article = await Article.findById(req.params.id)
+    if (article==null) res.redirect('/')
+    res.render('articles/show',{article: article})
+})
+
+router.post('/', async (req,res) => {
+    let article = new Article({
+	title: req.body.title,
+	description: req.body.description,
+	markdown: req.body.markdown
+    })
+    try{
+	article = await article.save()
+	res.redirect(`/articles/${article.id}`)
+    } catch (e) {
+	res.render('articles/new', {article:article})
+	console.log(e)
+    }
+    
+})
+
+router.delete('/:id',async(req,res) =>{
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
 })
 
 module.exports = router
--- webService/server.js
+++ webService/server.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+
 const app = express()
 
-app.set('view engine', 'ejs')
-app.use('/articles', articleRouter)
-
-app.get('/',(req,res) => {
-    res.render('articles/index')
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method')) 
+
+app.get('/',async (req,res) => {
+    const articles = await Article.find().sort({
+	createdAt:'desc'
+    })
+    res.render('articles/index',{articles:articles})
+})
+
+app.use('/articles', articleRouter)
 
 app.listen(5000)
