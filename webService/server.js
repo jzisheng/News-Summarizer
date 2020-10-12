c015a5b235c10266b8bfbe78cc27fbe3966ac90c
jzisheng <jzisheng@gmail.com>
2020-10-12 13:14:23 +0000

    addl push

--- webService/server.js
+++ webService/server.js
@@ -1,19 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-const articleRouter = require('routes/articles')
+const articleRouter = require('./routes/articles')
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
 
 app.get('/',(req,res) => {
-    const articles = [{
-	title:'article',
-	createdAt: Date.now(),
-	description: 'Test description'
-    }]
-    res.render('index', { articles: articles })
+    res.render('articles/index')
 })
 
 app.listen(5000)
